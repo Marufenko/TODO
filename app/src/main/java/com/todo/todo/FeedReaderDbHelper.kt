351ed9236af40ae1a5988d8ce4f2ff20f3a6d3be
price.of.the.flight@hotmail.com <hptor_i27Q2>
2018-07-09 21:54:16 +0200

    use SQLite to store data

---
+++ app/src/main/java/com/todo/todo/FeedReaderDbHelper.kt
@@ -0,0 +1,40 @@
+package com.todo.todo
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.provider.BaseColumns
+
+object FeedReaderContract {
+    object FeedEntry : BaseColumns {
+        const val TABLE_NAME = "test_table"
+        const val COLUMN_KEY = "key"
+        const val COLUMN_VALUE = "value"
+    }
+}
+
+private const val SQL_CREATE_ENTRIES =
+        "CREATE TABLE ${FeedReaderContract.FeedEntry.TABLE_NAME} (" +
+                "${BaseColumns._ID} INTEGER PRIMARY KEY," +
+                "${FeedReaderContract.FeedEntry.COLUMN_KEY} TEXT," +
+                "${FeedReaderContract.FeedEntry.COLUMN_VALUE} TEXT)"
+
+private const val SQL_DELETE_ENTRIES =
+        "DROP TABLE IF EXISTS ${FeedReaderContract.FeedEntry.TABLE_NAME}"
+
+class FeedReaderDbHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+
+    override fun onCreate(db: SQLiteDatabase) {
+        db.execSQL(SQL_CREATE_ENTRIES)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        db.execSQL(SQL_DELETE_ENTRIES)
+        onCreate(db)
+    }
+
+    companion object {
+        const val DATABASE_VERSION = 1
+        const val DATABASE_NAME = "FeedReader.db"
+    }
+}
--- app/src/main/java/com/todo/todo/MainActivity.kt
+++ app/src/main/java/com/todo/todo/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.todo.todo
 
-import android.annotation.SuppressLint
-import android.content.Context
+import android.content.ContentValues
 import android.os.Bundle
+import android.provider.BaseColumns
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.EditText
@@ -11,32 +11,69 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private val MY_PREFS_NAME = "MyPrefsFile"
-    private val MY_PREFS_KEY = "gaols"
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
     }
 
-    @SuppressLint("CommitPrefEdits")
-    fun saveString(view: View) {
-        val tv = view.rootView.findViewById<EditText>(R.id.editText)
-        val goal = tv.text.toString()
-        val text = GoalDataModel.addFirstGoal(goal)
+    fun putData(view: View) {
+        val keyInputField = view.rootView.findViewById<EditText>(R.id.inputKey)
+        val valueInputField = view.rootView.findViewById<EditText>(R.id.inputValue)
+        val key = keyInputField.text.toString()
+        val value = valueInputField.text.toString()
 
-        getSharedPreferences(MY_PREFS_NAME, Context.MODE_PRIVATE).edit().apply {
-            putString(MY_PREFS_KEY, text)
-            commit()
+        val context = applicationContext
+
+        // Access database
+        val dbHelper = FeedReaderDbHelper(context)
+        // Gets the data repository in write mode
+        val db = dbHelper.writableDatabase
+        // Create a new map of values, where column names are the keys
+        val values = ContentValues().apply {
+            put(FeedReaderContract.FeedEntry.COLUMN_KEY, key)
+            put(FeedReaderContract.FeedEntry.COLUMN_VALUE, value)
         }
+        // Insert the new row
+        db?.insert(FeedReaderContract.FeedEntry.TABLE_NAME, null, values)
     }
 
-    fun retrieveString(view: View) {
-        val prefs = getSharedPreferences(MY_PREFS_NAME, Context.MODE_PRIVATE)
-        val goals = prefs.getString(MY_PREFS_KEY, "no data")
+    fun getData(view: View) {
+        val context = applicationContext
+        // Access database
+        val dbHelper = FeedReaderDbHelper(context)
+        // Gets the data repository in read mode
+        val db = dbHelper.readableDatabase
+        // Define a projection that specifies which columns from the database fun actually use after query
+        val projection = arrayOf(
+                BaseColumns._ID,
+                FeedReaderContract.FeedEntry.COLUMN_KEY,
+                FeedReaderContract.FeedEntry.COLUMN_VALUE
+        )
+        // How you want the results sorted in the resulting Cursor
+        val sortOrder = "${BaseColumns._ID} DESC"
+        val cursor = db.query(
+                FeedReaderContract.FeedEntry.TABLE_NAME,   // The table to query
+                projection,                  // The array of columns to return (pass null to get all)
+                null,               // no WHERE statement
+                null,            // no values for WHERE statement
+                null,               // don't group the rows
+                null,                // don't filter by row groups
+                sortOrder                   // The sort order
+        )
 
-        val tv = view.rootView.findViewById<TextView>(R.id.textView)
-        tv.text = GoalDataModel.retrieveFirstGoal(goals)
+        // Access to data within Cursor
+        lateinit var itemKey: String
+        lateinit var itemValue: String
+        with(cursor) {
+            moveToNext()
+            itemKey = getString(getColumnIndexOrThrow(FeedReaderContract.FeedEntry.COLUMN_KEY))
+            itemValue = getString(getColumnIndexOrThrow(FeedReaderContract.FeedEntry.COLUMN_VALUE))
+        }
+
+        val keyOutputField = view.rootView.findViewById<TextView>(R.id.outputKey)
+        val valueOutputField = view.rootView.findViewById<TextView>(R.id.outputValue)
+        keyOutputField.text = itemKey
+        valueOutputField.text = itemValue
     }
 }
