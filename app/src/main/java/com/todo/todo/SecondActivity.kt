1e5abf965352813b11bb2830a65ddca04e9e3eb6
price.of.the.flight@hotmail.com <hptor_i27Q2>
2018-07-10 23:09:02 +0200

    add test recyclerView

--- app/src/main/java/com/todo/todo/SecondActivity.kt
+++ app/src/main/java/com/todo/todo/SecondActivity.kt
@@ -1,12 +1,49 @@
 package com.todo.todo
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 
 class SecondActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        val recyclerView: RecyclerView = findViewById(R.id.test_recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = Adapter(generateFakeValues())
+    }
+
+    private fun generateFakeValues(): List<String> {
+        val values = mutableListOf<String>()
+        for (i in 0..100) {
+            values.add("$i element")
+        }
+        return values
+    }
+
+    class Adapter(private val values: List<String>) : RecyclerView.Adapter<Adapter.ViewHolder>() {
+
+        override fun getItemCount() = values.size
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            val itemView = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.test_text_view, parent, false)
+            return ViewHolder(itemView)
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            holder.textView.text = values[position]
+        }
+
+        class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            var textView: TextView = itemView.findViewById(R.id.text_list_item)
+        }
     }
 }
