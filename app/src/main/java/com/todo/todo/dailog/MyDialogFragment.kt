1acec3c0bd5d5d2ee0e08039c21a39f65853f150
price.of.the.flight@hotmail.com <hptor_i27Q2>
2018-09-10 23:12:03 +0200

    Add record to db from dialog

--- app/src/main/java/com/todo/todo/dailog/MyDialogFragment.kt
+++ app/src/main/java/com/todo/todo/dailog/MyDialogFragment.kt
@@ -1,13 +1,21 @@
 package com.todo.todo.dailog
 
 import android.app.Dialog
+import android.content.ContentValues
 import android.os.Bundle
 import android.support.v4.app.DialogFragment
 import android.support.v4.app.FragmentManager
 import android.support.v7.app.AlertDialog
 import android.view.inputmethod.EditorInfo
 import android.widget.EditText
+import com.todo.todo.FeedReaderContract
+import com.todo.todo.FeedReaderDbHelper
+import com.todo.todo.MyApplication
 import com.todo.todo.R
+import android.widget.Toast
+import android.content.DialogInterface
+
+
 
 class MyDialogFragment : DialogFragment() {
 
@@ -29,7 +37,28 @@ class MyDialogFragment : DialogFragment() {
 
         return AlertDialog.Builder(activity!!)
                 .setTitle(title)
-                .setPositiveButton("OK", null)
+                .setPositiveButton("OK") { _, _ ->
+
+                    // add record to database
+                    val key =  goalInput.text.toString()
+                    val value = "0" // "0" by default as boolean value which represents not ticked goal
+
+                    // add item to database
+                    val context = MyApplication.appContext
+
+                    // Access database
+                    val dbHelper = FeedReaderDbHelper(context)
+                    // Gets the data repository in write mode
+                    val db = dbHelper.writableDatabase
+                    // Create a new map of values, where column names are the keys
+                    val values = ContentValues().apply {
+                        put(FeedReaderContract.FeedEntry.COLUMN_KEY, key)
+                        put(FeedReaderContract.FeedEntry.COLUMN_VALUE, value)
+                    }
+                    // Insert new entry to db
+                    db?.insert(FeedReaderContract.FeedEntry.TABLE_NAME, null, values)
+
+                }
                 .setNegativeButton("Cancel", null)
                 .setView(view)
                 .create()
@@ -40,8 +69,6 @@ class MyDialogFragment : DialogFragment() {
                 dialogTag = tag!!,
                 extras = Bundle().apply {
                     putString(KEY_NAME, goalInput.text.toString())
-
-                    //looks like adding record to db should be implemented here
                 }
         )
     }
